9fd9114499e933c080f098db2747fd66139392c0
curiyash <curiyash19@gmail.com>
2023-03-24 11:10:32 +0530

    Create Third party itineraries basic structure

--- src/ThirdItinerary.js
+++ src/ThirdItinerary.js
@@ -1,24 +1,29 @@
 import { Box, MenuItem, Select, TextField } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import sources from "./sources.json"
+import seasons from "./seasons.json"
+import MediaCard from "./PlaceCard";
 
 export default function ThirdItinerary() {
     // Form for User Input
     const [from, setFrom] = useState("");
     const [season, setSeason] = useState("");
 
-    function handleChange(e){
+    function handleFrom(e){
         setFrom(e.target.value);
     }
+
+    function handleSeason(e){
+        setSeason(e.target.value);
+    }
+
+    console.log(seasons);
 
     function Form() {
         return (
             <Box
                 component="form"
-                sx={{
-                    '& .MuiTextField-root': { m: 1, width: '25ch' },
-                }}
-                noValidate
+                noValidate  
                 autoComplete="off"
             >
                 <div>
@@ -26,8 +31,8 @@ export default function ThirdItinerary() {
                         labelId="demo-simple-select-label"
                         id="demo-simple-select"
                         value={from}
-                        label="Age"
-                        onChange={handleChange}
+                        label="Traveling from"
+                        onChange={handleFrom}
                     >
                         {sources.cityList.map((city) => {
                             return (
@@ -35,151 +40,57 @@ export default function ThirdItinerary() {
                             )
                         })}
                     </Select>
-                    <TextField
-                        id="outlined-disabled"
-                        label="Choose a season"
-                    />
-                    <TextField
-                        id="outlined-password-input"
-                        label="Budget in ₹"
-                        type="password"
-                        autoComplete="Your budget"
-                    />
-                    <TextField
-                        id="outlined-read-only-input"
-                        label="Read Only"
-                        defaultValue="Hello World"
-                        InputProps={{
-                            readOnly: true,
-                        }}
-                    />
-                    <TextField
-                        id="outlined-number"
-                        label="Number"
-                        type="number"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                    <TextField id="outlined-search" label="Search field" type="search" />
-                    <TextField
-                        id="outlined-helperText"
-                        label="Helper text"
-                        defaultValue="Default Value"
-                        helperText="Some important text"
-                    />
-                </div>
-                <div>
-                    <TextField
-                        required
-                        id="filled-required"
-                        label="Required"
-                        defaultValue="Hello World"
-                        variant="filled"
-                    />
-                    <TextField
-                        disabled
-                        id="filled-disabled"
-                        label="Disabled"
-                        defaultValue="Hello World"
-                        variant="filled"
-                    />
-                    <TextField
-                        id="filled-password-input"
-                        label="Password"
-                        type="password"
-                        autoComplete="current-password"
-                        variant="filled"
-                    />
-                    <TextField
-                        id="filled-read-only-input"
-                        label="Read Only"
-                        defaultValue="Hello World"
-                        InputProps={{
-                            readOnly: true,
-                        }}
-                        variant="filled"
-                    />
-                    <TextField
-                        id="filled-number"
-                        label="Number"
-                        type="number"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        variant="filled"
-                    />
-                    <TextField
-                        id="filled-search"
-                        label="Search field"
-                        type="search"
-                        variant="filled"
-                    />
-                    <TextField
-                        id="filled-helperText"
-                        label="Helper text"
-                        defaultValue="Default Value"
-                        helperText="Some important text"
-                        variant="filled"
-                    />
-                </div>
-                <div>
-                    <TextField
-                        required
-                        id="standard-required"
-                        label="Required"
-                        defaultValue="Hello World"
-                        variant="standard"
-                    />
-                    <TextField
-                        disabled
-                        id="standard-disabled"
-                        label="Disabled"
-                        defaultValue="Hello World"
-                        variant="standard"
-                    />
-                    <TextField
-                        id="standard-password-input"
-                        label="Password"
-                        type="password"
-                        autoComplete="current-password"
-                        variant="standard"
-                    />
-                    <TextField
-                        id="standard-read-only-input"
-                        label="Read Only"
-                        defaultValue="Hello World"
-                        InputProps={{
-                            readOnly: true,
-                        }}
-                        variant="standard"
-                    />
-                    <TextField
-                        id="standard-number"
-                        label="Number"
-                        type="number"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        variant="standard"
-                    />
-                    <TextField
-                        id="standard-search"
-                        label="Search field"
-                        type="search"
-                        variant="standard"
-                    />
-                    <TextField
-                        id="standard-helperText"
-                        label="Helper text"
-                        defaultValue="Default Value"
-                        helperText="Some important text"
-                        variant="standard"
-                    />
+                    <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        value={season}
+                        label="Season"
+                        onChange={handleSeason}
+                    >
+                        {Object.values(seasons).map((s) => {
+                            return (
+                                <MenuItem value={`${s.val}`}>{s.name}</MenuItem>
+                            )
+                        })}
+                    </Select>
                 </div>
             </Box>
         )
     }
+
+    console.log(from, season);
+
+    useEffect(() => {
+
+        async function getTrips(season, from){
+            await fetch("https://hubble.makemytrip.com/hubble/destListingFiltered", {
+                "credentials": "omit",
+                "headers": {
+                    "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/110.0",
+                    "Accept": "application/json",
+                    "Accept-Language": "en-US,en;q=0.5",
+                    "Content-Type": "application/json",
+                    "os": "desktop 1.0",
+                    "ver": "1.0",
+                    "deviceid": "59039f5f-c9e7-4a52-a255-ebceaed6c674",
+                    "region": "IN",
+                    "Sec-Fetch-Dest": "empty",
+                    "Sec-Fetch-Mode": "cors",
+                    "Sec-Fetch-Site": "same-site"
+                },
+                "referrer": "https://www.makemytrip.com/",
+                "body": "{\"autoSuggestTile\":null,\"queryId\":\"$4$$$$$$$$CTPNQ$\",\"schema\":\"id`ul`tt`tl`dt`td`tds`pr`ti`cp`sd`wl`prf`ct`sp\",\"delimiter\":\"`\",\"initial\":true,\"srcPoiId\":\"CTIXA\",\"appliedFilterValues\":{\"catIds\":[],\"selQueryCat\":null,\"budget\":null,\"time\":null,\"season\":null},\"notReadQueryId\":true,\"collectionId\":null,\"deeplink\":false,\"timestamp\":1679632917272}",
+                "method": "POST",
+                "mode": "cors"
+            }).then((res) => {
+                return res.json();
+            }).then((data) => {
+                console.log(data);
+            });
+        }
+
+        getTrips();
+    }, [from, season])
 
     return (
         <>
