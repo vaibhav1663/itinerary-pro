27e5b29e8eb4af86396fc75f1c293a3bf27e238c
ShyamR12 <shyamrandar12@gmail.com>
2023-03-24 17:34:53 +0530

    Created page for existing itinerary

--- src/App.js
+++ src/App.js
@@ -11,23 +11,25 @@ import AIConciseTrip from "./screens/AIConciseTrip";
 import Login from "./screens/Login";
 import { UserAuthContextProvider } from "./context/UserAuthContext";
 import ProtectedRoute from "./screens/ProtectedRoute";
+import ExistingItinerary from "./screens/ExistingItinerary";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <UserAuthContextProvider>
-        <Routes>
-          <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
-          <Route path="/about" element={<ProtectedRoute><About /></ProtectedRoute>} />
-          <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/DetailedTrip" element={<ProtectedRoute><AITravelPlannerDetailed /></ProtectedRoute>} />
-          <Route path="/ConciseTrip" element={<ProtectedRoute><AIConciseTrip /></ProtectedRoute>} />
-          <Route path="/third_itinerary" element={<ProtectedRoute><ThirdItinerary /></ProtectedRoute>} />
-          <Route path="/itinerary_details" element={<ProtectedRoute><ItineraryDetails /></ProtectedRoute>} />
-          <Route path="*" element={<NoPage />} />
-        </Routes>
+        <UserAuthContextProvider>
+          <Routes>
+            <Route path="/" element={<ProtectedRoute><Home /></ProtectedRoute>} />
+            <Route path="/about" element={<ProtectedRoute><About /></ProtectedRoute>} />
+            <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/DetailedTrip" element={<ProtectedRoute><AITravelPlannerDetailed /></ProtectedRoute>} />
+            <Route path="/ConciseTrip" element={<ProtectedRoute><AIConciseTrip /></ProtectedRoute>} />
+            <Route path="/third_itinerary" element={<ProtectedRoute><ThirdItinerary /></ProtectedRoute>} />
+            <Route path="/itinerary_details" element={<ProtectedRoute><ItineraryDetails /></ProtectedRoute>} />
+            <Route path="/existing_itinerary" element={<ProtectedRoute><ExistingItinerary /></ProtectedRoute>} />
+            <Route path="*" element={<NoPage />} />
+          </Routes>
         </UserAuthContextProvider>
       </BrowserRouter>
     </div>
