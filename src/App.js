829c16c98abe5514b9e621c527795e0246d39548
Kartik <kartikmandhan134@gmail.com>
2023-03-24 18:33:52 +0530

    AboutUs Page and minor improvements

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ function App() {
               }
             />
             <Route
-              path="/about"
+              path="/AboutUs"
               element={
                 <ProtectedRoute>
                   <About />
