67d54aa24df67229764ced4edded4874be131864
Kartik <kartikmandhan134@gmail.com>
2023-03-25 00:35:10 +0530

    chooseAI responsiveness

--- src/screens/AITravelPlanner.js
+++ src/screens/AITravelPlanner.js
@@ -97,7 +97,7 @@ const Title = styled.h1`
 `;
 
 const Subtitle = styled.h2`
-color:"#000",
+  color: "#000";
   font-size: 1rem;
   font-weight: normal;
   font-family: "Roboto", sans-serif;
@@ -1096,9 +1096,7 @@ const AITravelPlanner = () => {
         </Panel>
       </Container>
       {<MapShow title="Browse Map" dst={values.destinationCountry} />}
-      <div
-        className="relative  md:mt-6 bg-gradient-to-b from-emerald-200 to-white"
-      >
+      <div className="relative  md:mt-6 bg-gradient-to-b from-emerald-200 to-white">
         <div className="travigo-container" style={{ paddingBottom: "50px" }}>
           <div className="flex items-center justify-center text-center mb-11 md:mb-7">
             <h1 className="text-5xl lg:text-4xl md:text-3xl sm:text-2xl xsm:text-xl font-bold filter drop-shadow-lg text-slate-900">
@@ -1106,25 +1104,46 @@ const AITravelPlanner = () => {
             </h1>
           </div>
           <div className="d-flex items-center justify-center">
-            <div style={{display: "flex" }}>
-            {
-              weather.length === 0 ? <p className="text-lg xl:text-base sm:text-sm xsm:text-xs font-medium">Sorry weather not available for this city</p> :
+            <div style={{ display: "flex" }}>
+              {weather.length === 0 ? (
+                <p className="text-lg xl:text-base sm:text-sm xsm:text-xs font-medium">
+                  Sorry weather not available for this city
+                </p>
+              ) : (
                 weather.map((item) => (
-
-                  <div style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", width: "400px", padding: "7px", margin: "5px", border: "1px black solid", borderRadius: "8px" }}>
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      flexDirection: "column",
+                      width: "400px",
+                      padding: "7px",
+                      margin: "5px",
+                      border: "1px black solid",
+                      borderRadius: "8px",
+                    }}
+                  >
                     <h4>{item.date}</h4>
-                    <p className="text-lg xl:text-base sm:text-sm xsm:text-xs font-medium">{item.maxTemp + "/" + item.minTemp + "C"}</p>
-                    <img src={"https://developer.foreca.com/static/images/symbols/" + item.symbol + ".png"}></img>
-                    <p className="text-2xl xl:text-2xl sm:text-xl font-bold drop-shadow-lg">{item.symbolPhrase}</p>
+                    <p className="text-lg xl:text-base sm:text-sm xsm:text-xs font-medium">
+                      {item.maxTemp + "/" + item.minTemp + "C"}
+                    </p>
+                    <img
+                      src={
+                        "https://developer.foreca.com/static/images/symbols/" +
+                        item.symbol +
+                        ".png"
+                      }
+                    ></img>
+                    <p className="text-2xl xl:text-2xl sm:text-xl font-bold drop-shadow-lg">
+                      {item.symbolPhrase}
+                    </p>
                   </div>
                 ))
-              }
-        
+              )}
             </div>
           </div>
         </div>
-
-
       </div>
     </>
   );
