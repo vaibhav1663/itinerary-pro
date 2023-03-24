1148d6e9c33e606488a303f667ab81162ed4be2f
Kartik <kartikmandhan134@gmail.com>
2023-03-24 10:52:23 +0530

    restructure project

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import AIItinerary from './Itinerary';
-import ThirdItinerary from './ThirdItinerary';
-import ItineraryDetails from './ItineraryDetails';
-import Profile from './Profile';
-import NoPage from './NoPage';
-import About from './About';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./screens/Home";
+import AIItinerary from "./screens/Itinerary";
+import ThirdItinerary from "./screens/ThirdItinerary";
+import ItineraryDetails from "./screens/ItineraryDetails";
+import Profile from "./screens/Profile";
+import NoPage from "./screens/NoPage";
+import About from "./screens/About";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home /> }/>
-        <Route path="/about" element={<About />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/ai_itinerary" element={<AIItinerary />} />
-        <Route path="/third_itinerary" element={<ThirdItinerary />} />
-        <Route path="/itinerary_details" element={<ItineraryDetails />} />
-        <Route path="*" element={<NoPage />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/ai_itinerary" element={<AIItinerary />} />
+          <Route path="/third_itinerary" element={<ThirdItinerary />} />
+          <Route path="/itinerary_details" element={<ItineraryDetails />} />
+          <Route path="*" element={<NoPage />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
