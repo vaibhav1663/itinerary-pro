476ba7798ec980910ee9d1a9364d16f09a5f7de2
Kartik <kartikmandhan134@gmail.com>
2023-03-24 16:12:27 +0530

    Explore.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import Explore from "../components/Explore";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import {
   hero,
   navlinks,
   memory,
-  placesAPI,
   brands,
   pricingapi,
   bannerAPI,
@@ -17,6 +17,7 @@ export default function Home() {
     <>
       <Navbar navlinks={navlinks} />
       <Hero hero={hero} />
+      <Explore title="Explore The Beauty of World" />
     </>
   );
 }
