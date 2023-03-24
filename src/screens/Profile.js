dd5c784deee085788966600e15b54e50b311e22a
cout"vaibhav <85737835+vaibhav1663@users.noreply.github.com>
2023-03-24 15:41:41 +0530

    added login

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,9 +1,73 @@
+
 import React from "react";
+import { useNavigate } from "react-router";
+import { useUserAuth } from "../context/UserAuthContext";
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
 
 export default function Profile(){
-    return (
-        <>
-            <h1>Profile</h1>
-        </>
-    )
+    const { logOut, user } = useUserAuth();
+  const navigate = useNavigate();
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      navigate("/login");
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  return (
+    <>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }} src={user.photoURL}>
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Profile
+          </Typography>
+          <Box  noValidate sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              fullWidth
+              id="email"
+              name="email"
+              autoComplete="email"
+              disabled
+              value={user && user.email}
+            />
+            <TextField
+              margin="normal"
+              fullWidth
+              id="name"
+              value={user && user.displayName}
+              disabled
+            />
+            <Button
+              onClick={handleLogout}
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Logout
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </>
+  );
 }
